d93abcedfaf178cc2b17b79305763ffd5374417d
0qcomp <46693333+0qcomp@users.noreply.github.com>
2019-04-30 19:58:41 -0700

    Add files via upload

---
+++ hmm.py
@@ -0,0 +1,9 @@
+'''
+File: hmm.py
+Author: Justin Nichols
+Course: ISTA311
+Assignment: Comp4
+
+Purpose: reads in a sequence of observations and, using a given markov model,
+    produces the most likely sequence of states
+'''
